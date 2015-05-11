9544cb273de39855697f58d076485a32ba0b2410
Neil Fenton <neilff@gmail.com>
2015-05-11 19:43:54 -0400

    added build to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,67 @@
 var gulp = require('gulp-help')(require('gulp'));
+var eslint = require('gulp-eslint');
+var karma = require('gulp-karma');
+var connect = require('gulp-connect');
+var uglify = require('gulp-uglify');
+var ngAnnotate = require('gulp-ng-annotate');
+var rename = require('gulp-rename');
 
-gulp.task('test', 'Runs karma and lints code.', function () {
-  console.log('eslint + karma todo');
+gulp.task('connect', false, function() {
+  connect.server({
+    root: './',
+    livereload: true
+  });
+});
+
+gulp.task('html', false, function () {
+  gulp.src('./demo/*.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('js', false, function () {
+  gulp.src('./demo/*.js')
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', false, function () {
+  gulp.watch(['./demo/*.html'], ['html']);
+  gulp.watch(['./demo/*.js'], ['js']);
+});
+
+gulp.task('lint', false, function () {
+  return gulp.src(['src/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failOnError());
+});
+
+gulp.task('test', 'Runs karma and lints code.', ['lint'], function() {
+  var testFiles = [
+    'bower_components/jquery/dist/jquery.js',
+    'bower_components/angular/angular.js',
+    'bower_components/angular-mocks/angular-mocks.js',
+    'src/**/*.js',
+    'tests/**/*.js'
+  ];
+
+  return gulp.src(testFiles)
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'run'
+    }))
+    .on('error', function(err) {
+      throw err;
+    });
 });
 
 gulp.task('build', 'Builds project (concat, ngmin, uglify).', function () {
-  console.log('build todo');
+  return gulp.src('src/*.js')
+    .pipe(ngAnnotate())
+    .pipe(uglify())
+    .pipe(rename({
+      extname: '.min.js'
+    }))
+    .pipe(gulp.dest('build'));
 });
 
-gulp.task('dev', 'Launch dev environment for project.', function () {
-  console.log('dev todo');
-});
-
-gulp.task('changelog', 'Launch dev environment for project.', function () {
-  console.log('dev todo');
-});
+gulp.task('serve', 'Launch dev environment', ['connect', 'watch']);
